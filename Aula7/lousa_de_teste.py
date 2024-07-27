41c9186af74b450a17c91d3a698873fad1c17744
Yuri Santiago <153228868+yurios1@users.noreply.github.com>
2024-07-27 00:04:09 -0300

    Update lousa_de_teste.py

--- Aula7/lousa_de_teste.py
+++ Aula7/lousa_de_teste.py
@@ -1,1 +1,13 @@
-bnetnbebbbwbwrv
+from faker import Faker
+
+dicio = {}
+
+faker = Faker('pt_BR')
+
+idContador = 0
+while idContador < 101:
+    aluno = faker.name()
+    idContador += 1
+    dicio[idContador] = aluno
+
+print(dicio)
